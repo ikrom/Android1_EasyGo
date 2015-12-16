3f59f7d71ea6c729d6007c45c64eb4f82ae818c2
ikrom <ichrome.fahdi@gmail.com>
2015-12-16 15:37:52 +0700

    second

---
+++ EasyGo/app/src/main/java/tc/easygo/AsyncResponse.java
@@ -0,0 +1,7 @@
+package tc.easygo;
+
+/**
+ * Created by ikromaulia on 12/16/2015.
+ */
+public interface AsyncResponse {
+}
---
+++ EasyGo/app/src/main/java/tc/easygo/connection/PostRequest.java
@@ -0,0 +1,7 @@
+package tc.easygo.connection;
+
+/**
+ * Created by ikromaulia on 12/16/2015.
+ */
+public class PostRequest {
+}
