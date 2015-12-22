85b5e54edad82a536913b95675cd6f5459c7d641
Fahmy Thoriqul Haq <fahmythor@gmail.com>
2015-12-22 21:26:07 +0700

    Proses Login beres. Penambahan Preferences untuk session login Catatan : Setelah login jika menekan tombol back kembali pada halaman login. Karena belum ada tombol logout.

---
+++ EasyGo/app/src/main/java/tc/easygo/CheckPreferenceActivity.java
@@ -0,0 +1,41 @@
+package tc.easygo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class CheckPreferenceActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
+    public static final String MyPREFERENCES = "MyPrefs" ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_preference);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        sharedPreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        CheckPreference();
+
+    }
+    private void CheckPreference() {
+        Boolean cekStatus = sharedPreferences.getBoolean("Status", false);
+
+        if(cekStatus.equals(true)) {
+            Intent intent = new Intent(CheckPreferenceActivity.this,HomeActivity.class);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(CheckPreferenceActivity.this,LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+
+}
