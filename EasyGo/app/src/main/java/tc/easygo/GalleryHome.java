b35c94131004f2fbd61cf58fcdb2435f227acebe
ikrom <ichrome.fahdi@gmail.com>
2015-12-31 08:02:15 +0700

    a

--- EasyGo/app/src/main/java/tc/easygo/GalleryHome.java
+++ EasyGo/app/src/main/java/tc/easygo/GalleryHome.java
@@ -13,6 +13,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -20,6 +21,7 @@ import android.widget.RatingBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.etsy.android.grid.StaggeredGridView;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
@@ -40,9 +42,24 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import tc.easygo.adapter.ItemGridAdapter;
 import tc.easygo.models.PopularModel;
 
 public class GalleryHome extends AppCompatActivity {
+
+    private StaggeredGridView staggeredGridView;
+    private String[] items = new String[]{
+            "https://s-media-cache-ak0.pinimg.com/736x/31/72/90/3172904c6591b127ac71b66b300a87f4.jpg",
+            "https://s-media-cache-ak0.pinimg.com/736x/c4/93/d1/c493d196b1b4e0ff61d8cb4a13727916.jpg",
+            "https://s-media-cache-ak0.pinimg.com/736x/54/a3/23/54a323dcafda85b7fda6883c00f2860a.jpg",
+            "https://s-media-cache-ak0.pinimg.com/736x/80/d5/b8/80d5b8ccfef474f5c895400acdbea63b.jpg",
+            "https://s-media-cache-ak0.pinimg.com/736x/0e/d7/7a/0ed77a3208f982a6f552fbf30f7d4a4c.jpg",
+            "https://s-media-cache-ak0.pinimg.com/736x/7f/2e/79/7f2e790b6ebb34699b887181d7a6a3ed.jpg",
+            "https://s-media-cache-ak0.pinimg.com/736x/fd/66/f0/fd66f045d95449469fa8b41b4b975edb.jpg",
+            "https://s-media-cache-ak0.pinimg.com/736x/d7/57/ca/d757ca1df5224dc46633a42e3c34bb90.jpg",
+            "https://s-media-cache-ak0.pinimg.com/736x/96/2a/d1/962ad10951a221f4ca26bb08e911da10.jpg",
+            "https://s-media-cache-ak0.pinimg.com/736x/ac/89/c7/ac89c7aaef5754bfd5d7b4a698fdb6cc.jpg"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +85,16 @@ public class GalleryHome extends AppCompatActivity {
 
         //JSON
         //new JSONTask().execute("http://navits.esy.es/index.php/services/getallgallery");
+
+        staggeredGridView = (StaggeredGridView)findViewById(R.id.gv_staggered);
+        ItemGridAdapter itemGridAdapter = new ItemGridAdapter(GalleryHome.this, items);
+        staggeredGridView.setAdapter(itemGridAdapter);
+        staggeredGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                DetailImageActivity.toDetailImageActivity(GalleryHome.this, items[position]);
+            }
+        });
 
     }
 
